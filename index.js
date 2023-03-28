ba076305eeb46827af49600e00062e2024ae3f4c
Alex-vell <sanyavelichko27@gmail.com>
2023-03-28 14:32:26 +0300

    rm

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL || "http://localhost:3000"
+    origin: process.env.CLIENT_URL
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
